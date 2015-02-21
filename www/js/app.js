f5465a675f8185b188395ca20fcb6e62144c7245
Baptiste Pernet <bapt@sp4ce.net>
2015-02-21 17:11:43 -0500

    Playing with ionic

--- www/js/app.js
+++ www/js/app.js
@@ -1,102 +1,64 @@
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'macholand' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+// 'macholand.services' is found in services.js
+// 'macholand.controllers' is found in controllers.js
+angular.module('macholand', ['ionic', 'macholand.controllers', 'macholand.services'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
-  // setup an abstract state for the tabs directive
+    // setup an abstract state for the tabs directive
     .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
+        url: "/tab",
+        abstract: true,
+        templateUrl: "templates/tabs.html"
     })
 
-  .state('tab.friends', {
-      url: '/friends',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/tab-friends.html',
-          controller: 'FriendsCtrl'
-        }
-      }
-    })
-    .state('tab.friend-detail', {
-      url: '/friend/:friendId',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/friend-detail.html',
-          controller: 'FriendDetailCtrl'
-        }
-      }
+    // Each tab has its own nav history stack:
+
+    .state('tab.camera', {
+        url: '/camera',
+        views: {
+            'tab-camera': {
+                templateUrl: 'templates/tab-camera.html',
+                controller: 'CameraCtrl',
+            },
+        },
     })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+    .state('tab.about', {
+        url: '/about',
+        views: {
+            'tab-about': {
+                templateUrl: 'templates/tab-about.html',
+                controller: 'AboutCtrl',
+            },
+        },
+    });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/camera');
 
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,41 @@
-angular.module('starter.controllers', [])
+angular.module('macholand.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controlller('AboutCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
+.controller('CameraCtrl', function($scope) {
+//    $scope.takePic = function() {
+//        navigator.camera.getPicture(onSuccess, onFail, {
+//            quality: 50,
+//            destinationType: Camera.DestinationType.FILE_URI,
+//            sourceType: 1,
+//            encodingType: 0
+//        });
+//    }
+//
+//    var onSuccess = function(FILE_URI) {
+//        console.log(FILE_URI);
+//        $scope.picData = FILE_URI;
+//        $scope.$apply();
+//    };
+//
+//    var onFail = function(e) {
+//        console.log("On fail " + e);
+//    }
+//
+//    $scope.send = function() {
+//        var myImg = $scope.picData;
+//        var options = new FileUploadOptions();
+//        options.fileKey="post";
+//        options.chunkedMode = false;
+//        var params = {};
+//        params.user_token = localStorage.getItem('auth_token');
+//        params.user_email = localStorage.getItem('email');
+//        options.params = params;
+//        var ft = new FileTransfer();
+//        ft.upload(
+//            myImg,
+//            encodeURI("https://example.com/posts/"),
+//            onUploadSuccess, onUploadFail, options);
+//    }
 })
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
-
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+;
